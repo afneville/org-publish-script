bb99b0cc765b375213a449706f9a40ababed6176
Alexander Neville <git@afneville.com>
2023-04-02 22:58:11 +0100

    create Dockerfile and src submodule

--- export.sh
+++ export.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-
 rm -rf ./out
 mkdir -p out/ out/html out/latex
 mkdir -p out/html/res
 mkdir -p out/html/blog/res
-
 emacs -q --script publish.el
+docker build -t website-test .
